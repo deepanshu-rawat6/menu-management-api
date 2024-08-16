61428ab52876f06bb853dca31c65217eb3f18245
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-08-17 02:51:22 +0530

    UPDATE: basic server settings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import App from './server';
 import http from 'http';
 import config from './config';
-import { logger, swaggerDocs } from './utils';
+import logger from './utils/logger';
+import swaggerDocs from './utils/swagger-docs';
+import { connect } from './utils/db';
 
 const server = http.createServer(App);
 
 server.listen(config.PORT, () => {
+
+    connect();
 
     logger.info(`Server is running on port: ${config.PORT}`);
 
