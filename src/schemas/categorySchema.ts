9433c8c2ce4757370b0d4edbf805174d9c1189c5
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-08-17 18:10:08 +0530

    UPDATE: category schema for zod validation

--- src/schemas/categorySchema.ts
+++ src/schemas/categorySchema.ts
@@ -5,8 +5,16 @@ export const createCategorySchema = z.object({
     image: z.string().url(),
     description: z.string(),
     tax_applicable: z.boolean(),
-    tax: z.number(),
-    tax_type: z.string()
+    tax: z.number().optional(),
+    tax_type: z.string().optional()
+}).refine(data => {
+    if (data.tax_applicable) {
+        return data.tax !== undefined && data.tax_type !== undefined;
+    }
+    return true;
+}, {
+    message: 'Tax and tax type are required when tax is applicable',
+    path: ['tax', 'tax_type']
 })
 
 export const updateCategorySchema = z.object({
@@ -14,6 +22,14 @@ export const updateCategorySchema = z.object({
     image: z.string().url(),
     description: z.string(),
     tax_applicable: z.boolean(),
-    tax: z.number(),
-    tax_type: z.string()
-})
+    tax: z.number().optional(),
+    tax_type: z.string().optional()
+}).refine(data => {
+    if (data.tax_applicable) {
+        return data.tax !== undefined && data.tax_type !== undefined;
+    }
+    return true;
+}, {
+    message: 'Tax and tax type are required when tax is applicable',
+    path: ['tax', 'tax_type']
+});
