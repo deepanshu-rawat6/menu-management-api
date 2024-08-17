3bab05fab9e3f51b00681ca2bedced49594c2def
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-08-17 19:35:42 +0530

    UPDATE: validation check for sub-categories

--- src/schemas/subCategorySchema.ts
+++ src/schemas/subCategorySchema.ts
@@ -5,7 +5,15 @@ export const createSubCategorySchema = z.object({
     image: z.string().url(),
     description: z.string(),
     tax_applicable: z.boolean(),
-    tax: z.number(),
+    tax: z.number().optional(),
+}).refine(data => {
+    if (data.tax_applicable) {
+        return data.tax !== undefined;
+    }
+    return true;
+}, {
+    message: 'Tax is required when tax is applicable',
+    path: ['tax']
 })
 
 export const updateSubCategorySchema = z.object({
@@ -13,5 +21,13 @@ export const updateSubCategorySchema = z.object({
     image: z.string().url(),
     description: z.string(),
     tax_applicable: z.boolean(),
-    tax: z.number(),
-})
+    tax: z.number().optional(),
+}).refine(data => {
+    if (data.tax_applicable) {
+        return data.tax !== undefined;
+    }
+    return true;
+}, {
+    message: 'Tax is required when tax is applicable',
+    path: ['tax']
+});
