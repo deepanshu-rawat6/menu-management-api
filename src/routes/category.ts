ad3bf056c0c487f597ccc5237ed3877fdfd2b04c
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-08-17 18:11:54 +0530

    DEV: routes and swagger configs for categories

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -3,115 +3,355 @@ import logger from '../utils/logger';
 import { StatusCodes } from 'http-status-codes';
 import { validateData } from '../middlewares';
 import { createCategorySchema, updateCategorySchema } from '../schemas';
-import { } from '../controllers';
+import { createCategory, getAllCategories, getCategoryByIdORName, updateCategory } from '../controllers';
 
 export default (router: express.Router) => {
     /**
      * @openapi
-     * '/api/v1/category':
+     * '/api/v1/categories':
      *   post:
      *     tags:
      *       - Category
-     *     description: Create the category with the given details
+     *     summary: Create a new category
+     *     description: Create a category with the given details
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               name:
+     *                 type: string
+     *                 description: The name of the category
+     *                 example: "Electronics"
+     *               image:
+     *                 type: string
+     *                 format: url
+     *                 description: The image URL of the category
+     *                 example: "https://example.com/image.jpg"
+     *               description:
+     *                 type: string
+     *                 description: The description of the category
+     *                 example: "Category for all electronic items"
+     *               tax_applicable:
+     *                 type: boolean
+     *                 description: The tax applicable status of the category
+     *                 example: true
+     *               tax:
+     *                 type: number
+     *                 description: The tax percentage of the category
+     *                 example: 18
+     *               tax_type:
+     *                 type: string
+     *                 description: The tax type of the category
+     *                 example: "GST"
      *     responses:
      *       200:
-     *         description: Created the category successfully
+     *         description: Category data fetched successfully
+     *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The success message
+ *                   example: "Category created successfully"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: The unique identifier of the category
+ *                       example: "12345"
+ *                     name:
+ *                       type: string
+ *                       description: The name of the category
+ *                       example: "Electronics"
+ *                     image:
+ *                       type: string
+ *                       format: url
+ *                       description: The image URL of the category
+ *                       example: "https://example.com/image.jpg"
+ *                     description:
+ *                       type: string
+ *                       description: The description of the category
+ *                       example: "Category for all electronic items"
+ *                     tax_applicable:
+ *                       type: boolean
+ *                       description: Whether the category is subject to tax
+ *                       example: true
+ *                     tax:
+ *                       type: number
+ *                       description: The tax percentage applicable to the category
+ *                       example: 18
+ *                     tax_type:
+ *                       type: string
+ *                       description: The type of tax applicable
+ *                       example: "GST"
+     *       400:
+     *         description: Bad request, some details are missing or invalid
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   description: The error message
+     *                   example: "name is missing"
+     *       409:
+     *         description: Conflict, failed to create category, category already exists
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   description: The error message
+     *                   example: "Category already exists"
      *       500:
      *         description: Internal server error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   description: The error message
+     *                   example: "Internal server error"
      */
-    router.post('/api/v1/category', validateData(createCategorySchema), (req: Request, res: Response) => {
-        logger.info('POST /api/v1/category');
+    router.post('/api/v1/categories', validateData(createCategorySchema), createCategory, (req: Request, res: Response) => {
+        logger.info('POST /api/v1/categories');
         res.status(StatusCodes.OK).send({
-            message: "POST /api/v1/category"
+            message: "POST /api/v1/categories"
         });
     });
 
-
     /**
      * @openapi
-     * '/api/v1/products/:id':
+     * '/api/v1/categories/{id}':
      *   get:
      *     tags:
-     *       - Products
-     *     description: To get a single product by id
+     *       - Category
+     *     summary: Retrieve a category by ID or name
+     *     description: This endpoint allows you to fetch a single category by either its ID or name.
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: string
+     *         description: The ID of the category to retrieve
+     *         example: "12345"
+     *       - in: query
+     *         name: name
+     *         required: false
+     *         schema:
+     *           type: string
+     *         description: The name of the category to retrieve
+     *         example: "Electronics"
      *     responses:
      *       200:
-     *         description: Product data fetched successfully
-     *       401:
-     *         description: Invalid credentials
+     *         description: Category data fetched successfully
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 id:
+     *                   type: string
+     *                   description: The unique identifier of the category
+     *                   example: "12345"
+     *                 name:
+     *                   type: string
+     *                   description: The name of the category
+     *                   example: "Electronics"
+     *                 image:
+     *                   type: string
+     *                   format: url
+     *                   description: The image URL of the category
+     *                   example: "https://example.com/image.jpg"
+     *                 description:
+     *                   type: string
+     *                   description: The description of the category
+     *                   example: "Category for all electronic items"
+     *                 tax_applicable:
+     *                   type: boolean
+     *                   description: Whether the category is subject to tax
+     *                   example: true
+     *                 tax:
+     *                   type: number
+     *                   description: The tax percentage applicable to the category
+     *                   example: 18
+     *                 tax_type:
+     *                   type: string
+     *                   description: The type of tax applicable
+     *                   example: "GST"
+     *       400:
+     *         description: Bad request, id or name not provided
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   description: The error message
+     *                   example: "Please provide either an ID or a name"
+     *       404:
+     *         description: Category not found
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   description: The error message
+     *                   example: "Category not found"
      *       500:
      *         description: Internal server error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   description: The error message
+     *                   example: "Internal server error"
      */
-    router.get('/api/v1/products/:id', (req: Request, res: Response) => {
-        logger.info('GET /api/v1/products/:id');
+    router.get('/api/v1/categories/:id', getCategoryByIdORName, (req: Request, res: Response) => {
+        logger.info('GET /api/v1/categories/:id');
         res.status(StatusCodes.OK).send({
-            message: "GET /api/v1/products/:id"
+            message: "GET /api/v1/categories/:id"
         });
     });
 
     /**
      * @openapi
-     * '/api/v1/products/':
+     * '/api/v1/categories/all':
      *   get:
      *     tags:
-     *       - Products
-     *     description: To get multiple products
+     *       - Category
+     *     summary: Retrieve all categories
+     *     description: This endpoint fetches all categories available in the system.
      *     responses:
      *       200:
-     *         description: Products data fetched successfully
-     *       401:
-     *         description: Invalid credentials
+     *         description: Categories data fetched successfully
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   id:
+     *                     type: string
+     *                     description: The unique identifier of the category
+     *                     example: "12345"
+     *                   name:
+     *                     type: string
+     *                     description: The name of the category
+     *                     example: "Electronics"
+     *                   image:
+     *                     type: string
+     *                     format: url
+     *                     description: The image URL of the category
+     *                     example: "https://example.com/image.jpg"
+     *                   description:
+     *                     type: string
+     *                     description: The description of the category
+     *                     example: "Category for all electronic items"
+     *                   tax_applicable:
+     *                     type: boolean
+     *                     description: Whether the category is subject to tax
+     *                     example: true
+     *                   tax:
+     *                     type: number
+     *                     description: The tax percentage applicable to the category
+     *                     example: 18
+     *                   tax_type:
+     *                     type: string
+     *                     description: The type of tax applicable
+     *                     example: "GST"
      *       500:
      *         description: Internal server error
      */
-    router.get('/api/v1/products/', (req: Request, res: Response) => {
-        logger.info('GET /api/v1/products/');
+    router.get('/api/v1/categories/all', getAllCategories, (req: Request, res: Response) => {
+        logger.info('GET /api/v1/catgories/all');
         res.status(StatusCodes.OK).send({
-            message: "GET /api/v1/products/"
+            message: "GET /api/v1/categories/all"
         });
     });
 
+
     /**
      * @openapi
-     * '/api/auth/products/:id':
+     * '/api/v1/categories/:id':
      *   patch:
      *     tags:
-     *       - Products
-     *     description: Update products with the given details
+     *       - Category
+     *     summary: Update a category by ID
+     *     description: Update a category with the given details by ID
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: string
+     *         description: The ID of the category to update
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               name:
+     *                 type: string
+     *                 description: The name of the category
+     *                 example: "Electronics"
+     *               image:
+     *                 type: string
+     *                 format: url
+     *                 description: The image URL of the category
+     *                 example: "https://example.com/image.jpg"
+     *               description:
+     *                 type: string
+     *                 description: The description of the category
+     *                 example: "Category for all electronic items"
+     *               tax_applicable:
+     *                 type: boolean
+     *                 description: The tax applicable status of the category
+     *                 example: true
+     *               tax:
+     *                 type: number
+     *                 description: The tax percentage of the category
+     *                 example: 18
+     *               tax_type:
+     *                 type: string
+     *                 description: The tax type of the category
+     *                 example: "GST"
      *     responses:
      *       200:
-     *         description: Product update successful
-     *       401:
-     *         description: Invalid credentials
+     *         description: Updated the category successfully
+     *       400:
+     *         description: Bad request, invalid data or missing id
+     *       404:
+     *         description: Category not found
      *       500:
      *         description: Internal server error
      */
-    router.patch('/api/v1/products/:id', validateData(updateCategorySchema), (req: Request, res: Response) => {
-        logger.info('PATCH /api/v1/products/:id');
+    router.patch('/api/v1/categories/:id', validateData(updateCategorySchema), updateCategory, (req: Request, res: Response) => {
+        logger.info('PATCH /api/v1/categories/:id');
         res.status(StatusCodes.OK).send({
-            message: "PATCH /api/v1/products/:id"
+            message: "PATCH /api/v1/categories/:id"
         });
     });
 
-    /**
-     * @openapi
-     * '/api/v1/products/:id':
-     *   delete:
-     *     tags:
-     *       - Products
-     *     description: Delete the product with the given id
-     *     responses:
-     *       200:
-     *         description: Product delete successfully
-     *       401:
-     *         description: Invalid credentials
-     *       500:
-     *         description: Internal server error
-     */
-    router.delete('/api/v1/products/:id', (req: Request, res: Response) => {
-        logger.info('DELETE /api/v1/products/:id');
-        res.status(StatusCodes.OK).send({
-            message: "DELETE /api/v1/products/:id"
-        });
-    });
 }
