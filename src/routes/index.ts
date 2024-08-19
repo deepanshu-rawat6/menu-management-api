21f8a77f0ae72bc0ba359c238b74748bde5d3101
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-08-20 01:01:04 +0530

    FIX: imports from items and sub-categories

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 
 import api from './api';
 import category from './category';
+import subCategory from './subCategory';
+import items from './items';
 
 const router = express.Router();
 
 export default (): express.Router => {
     api(router)
     category(router)
+    subCategory(router)
+    items(router)
 
     return router;
 }
